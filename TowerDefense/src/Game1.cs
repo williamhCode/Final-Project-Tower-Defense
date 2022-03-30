645e817ac7abdc905993fda9a31f308ad2a40d4a
shadowtyger05 <89882864+shadowtyger05@users.noreply.github.com>
2022-03-30 08:50:37 +0800

    adding models

--- TowerDefense/src/Game1.cs
+++ TowerDefense/src/Game1.cs
@@ -80,7 +80,7 @@ namespace TowerDefense
 
             Mouse.SetPosition((int)mouseDefaultPos.X, (int)mouseDefaultPos.Y);
             Content.RootDirectory = "Content/Models";
-            model = Content.Load<Model>("BuffingTower");
+            model = Content.Load<Model>("Buffing");
 
             camera = new Camera2D(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
 
